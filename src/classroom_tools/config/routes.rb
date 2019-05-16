67683b83eec21247eef68848ad7f2c31234550ff
Brandon Lam <brandonywlam@gmail.com>
2019-05-15 20:39:45 -0700

    configure routes

--- src/classroom_tools/config/routes.rb
+++ src/classroom_tools/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :school, only: [:index, :show, :create, :destroy] do
+        resource :students, only: [:index, :show, :create, :destroy]
+        resource :classroom, only: [:create, :destroy] do
+          post :randomizer
+        end
+      end
+    end
+  end
 end
