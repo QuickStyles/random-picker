1b3c6776237713ea312216820254cf086fc216ba
Brandon Lam <brandonywlam@gmail.com>
2019-05-15 20:24:17 -0700

    add associations to model, add seed file

--- src/classroom_tools/app/models/school.rb
+++ src/classroom_tools/app/models/school.rb
@@ -1,2 +1,5 @@
 class School < ApplicationRecord
+  has_secure_password
+  has_many :classrooms
+  has_many :students, through: :classrooms
 end
--- src/classroom_tools/db/seeds.rb
+++ src/classroom_tools/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ['jon', 'benjen', 'robert', 'syrio', 'eddard', 'viserys', 'drogo', 'renly', 'qhorin', 'jeor', 'beric', 'robb', 'catelyn', 'jeoffery', 'lysa', 'oberyn', 'ygritte', 'shae', 'tywin', 'mance', 'barristan', 'shireen', 'stannis', 'jon', 'roose', 'alliser', 'olle', 'hodor', 'brynden', 'rickon', 'ramsay', 'loras', 'mace', 'kevan', 'margaery']
+2.times do |x|
+  school = School.create({
+    name: "CodeCore College #{x}",
+    location: "142 W Hastings St, Vancouver, BC V6B 1G8",
+    account_id: "cc_#{x}",
+    password: "supersecret",
+    password_confirmation: "supersecret",
+    email: "codecore_#{x}@codecore.ca"
+  })
+  
+  for value in ['wolf', 'salmon', 'flower', 'kraken', 'deer', 'lion', 'snake', 'dragon', 'sunspear'] do
+    classroom = Classroom.create({
+      alias: value,
+      school: school
+    })
+    for name in names.sample(20)  do
+      Student.create({
+        name: name,
+        email: "#{name}@gmail.com",
+        mark: rand(0.1...4.0),
+        classroom:classroom
+      })
+    end
+  end
+end
